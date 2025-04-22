9ef0d586c69a0d73855d0283ebf82e5f2baeb693
Anwar Hossain <mahedianwar@gmail.com>
2025-04-23 00:31:54 +0600

    setup otp, mal, registration, schema...

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { ValidationError } from "../../../../packages/error-handler";
+import prisma from "../../../../packages/libs/prisma";
+import {
+  checkOtpRestriction,
+  sentOTP,
+  trackOtpRequest,
+  validateRegistrationData,
+} from "../utils/auth.helper";
+
+export const userRegistration = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    validateRegistrationData(req.body, "user");
+    const { name, email } = req.body;
+    //  password, phone_number, country
+    const userExist = await prisma.users.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userExist)
+      return next(new ValidationError("User already exist with this email"));
+
+    await checkOtpRestriction(email, next);
+    await trackOtpRequest(email, next);
+    await sentOTP(name, email, "user-activation-email", "Verify Your Email!");
+    res.status(200).json({ message: "OTP sent to email, please verify" });
+  } catch (error: any) {
+    return next(error);
+  }
+};
--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,22 +1,34 @@
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 import { errorMiddleware } from "../../../packages/error-handler/error-middleware";
+import AuthRouter from "./routes/auth.routes";
+dotenv.config();
 
+// @ts-ignore
+import swaggerDocument from "./swagger-output.json";
 const host = process.env.HOST ?? "localhost";
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send({ message: "Auth Service is healthy" });
 });
-
-app.use(express.json());
-app.use(cookieParser());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/docs-json", (req, res) => {
+  res.send(swaggerDocument);
+});
+app.use("/api", AuthRouter);
 app.use(errorMiddleware);
 
 const server = app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
+  console.log(`[ ready ] http://localhost:${port}/api-docs`);
 });
 
 server.on("error", console.error);
