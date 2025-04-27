6ddc73d6337cb185b9768524d9e832b73643065c
Anwar Hossain <mahedianwar@gmail.com>
2025-04-27 22:53:15 +0600

    feat: Add ImageKit integration for image management and processing

---
+++ packages/libs/imagekit/index.ts
@@ -0,0 +1,7 @@
+import imagekit from "imagekit";
+
+export const imageKit = new imagekit({
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY as string,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
+  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT as string,
+});
